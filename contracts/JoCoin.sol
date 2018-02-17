d41385b7c77ccb9a1875d36d4ccc4332953efd75
Joseph Sungpil Choi <chousemath@gmail.com>
2018-02-17 17:20:51 +0900

    Add a error logger

--- contracts/JoCoin.sol
+++ contracts/JoCoin.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.4.18;
 contract JoCoin {
     // public store of all balances
     mapping (address => uint) balances;
+    address public creator; // the exchange owner will be the one to create the coin
+    address public owner; // the owner is the freelance worker for whom the coin was created
     string public name;
     string public symbol;
     uint public decimals;
+    uint public price; // price that user wants to charge (KRW/coin)
 
     // define initial supply tokens to the creator of the contract
     // after following all the steps in the README file
@@ -16,15 +19,28 @@ contract JoCoin {
     function JoCoin(uint256 initialSupply) public {
         // give the creator all of the initial supply
         balances[msg.sender] = initialSupply;
+        creator = msg.sender;
         name = "JoCoin";
         symbol = "JCN";
         decimals = 2;
     }
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event ErrorMessage(address currentUser, string errorType, string errorMessage);
+
+    function setOwner(address coinOwner) public returns (bool success) {
+        if (msg.sender == creator) {
+            owner = coinOwner;
+            return true;
+        } else {
+            ErrorMessage(msg.sender, "setOwner", "msg.sender is not contract creator");
+            return false;
+        }
+    }
 
 	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
 		if (balances[msg.sender] < amount) {
+            ErrorMessage(msg.sender, "sendCoin", "Not enough coin in balance");
             return false;
         }
 		balances[msg.sender] -= amount;
--- test/TestJoCoin.sol
+++ test/TestJoCoin.sol
@@ -7,13 +7,18 @@ import "../contracts/JoCoin.sol";
 contract TestJoCoin {
 
     JoCoin jocoin = JoCoin(DeployedAddresses.JoCoin());
-    
+    address creator;
     address user2 = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
 
     function testCreatorGetsInitialSupply() public {
         uint expectedInitialSupply = 10000000000000000000000;
         uint initialSupply = jocoin.getBalance(msg.sender);
+        creator = msg.sender;
         Assert.equal(initialSupply, expectedInitialSupply, "Creator should get entirety of initial supply");
+    }
+
+    function testOwnerAssignment() public {
+        Assert.equal(jocoin.creator(), msg.sender, "Creator of the coin should be assigned as the owner");
     }
 
     function testUserCanSendCoin() public {
@@ -21,9 +26,14 @@ contract TestJoCoin {
         uint value = 100;
         uint expectedSenderBalance = initialSupply - value;
         uint expectedReceiverBalance = value;
-        if (jocoin.sendCoin(user2, value)) {
-            Assert.equal(jocoin.getBalance(msg.sender), expectedSenderBalance, "Correct amount should be deducted from sender's balance");
-            Assert.equal(jocoin.getBalance(user2), expectedReceiverBalance, "Correct ammount should be added to receiver's balance");   
-        }
+        // sendCoin(address receiver, uint amount)
+        jocoin.sendCoin(user2, value);
+        Assert.equal(jocoin.getBalance(msg.sender), expectedSenderBalance, "Correct amount should be deducted from sender's balance");
+        Assert.equal(jocoin.getBalance(user2), expectedReceiverBalance, "Correct ammount should be added to receiver's balance"); 
+    }
+
+    function testCreatorCanSetOwner() public {
+        jocoin.setOwner(user2);
+        Assert.equal(jocoin.owner(), user2, "Creator of the coin should be able to set the owner of the coin");
     }
 }
