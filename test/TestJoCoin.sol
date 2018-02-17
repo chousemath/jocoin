b0856daa90693aded80a786c0aea6ddc659d2cfb
Joseph Sungpil Choi <chousemath@gmail.com>
2018-02-17 15:45:50 +0900

    Make sure that sendCoin actually transfers funds from one account into another

--- test/TestJoCoin.sol
+++ test/TestJoCoin.sol
@@ -8,9 +8,22 @@ contract TestJoCoin {
 
     JoCoin jocoin = JoCoin(DeployedAddresses.JoCoin());
     
+    address user2 = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
+
     function testCreatorGetsInitialSupply() public {
         uint expectedInitialSupply = 10000000000000000000000;
         uint initialSupply = jocoin.getBalance(msg.sender);
         Assert.equal(initialSupply, expectedInitialSupply, "Creator should get entirety of initial supply");
+    }
+
+    function testUserCanSendCoin() public {
+        uint initialSupply = 10000000000000000000000;
+        uint value = 100;
+        uint expectedSenderBalance = initialSupply - value;
+        uint expectedReceiverBalance = value;
+        if (jocoin.sendCoin(user2, value)) {
+            Assert.equal(jocoin.getBalance(msg.sender), expectedSenderBalance, "Correct amount should be deducted from sender's balance");
+            Assert.equal(jocoin.getBalance(user2), expectedReceiverBalance, "Correct ammount should be added to receiver's balance");   
+        }
     }
 }
